3be04757712c25736258d8a8584907fef930cec5
Nayrus <kami.agofem@yahoo.de>
2024-12-29 14:02:10 +0100

    Added Note Waker basic function Moved useItemOn logic from ANB to items as a shift right click would not call it

--- src/main/java/net/nayrus/betterbeats/block/AdvancedNoteblock.java
+++ src/main/java/net/nayrus/betterbeats/block/AdvancedNoteblock.java
@@ -3,9 +3,11 @@ package net.nayrus.betterbeats.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -24,6 +26,8 @@ public class AdvancedNoteblock extends NoteBlock
 
     public static final int MAX_SUBTICKS = (int) (100 / SubTickScheduler.SUBTICK_LENGTH);
     public static final IntegerProperty SUBTICK = IntegerProperty.create("subtick",0,MAX_SUBTICKS-1);
+    public static final String[] NOTE_STRING = {"F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4",
+            "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5"};
 
     public AdvancedNoteblock(Properties properties) {
         super(properties);
@@ -48,27 +52,34 @@ public class AdvancedNoteblock extends NoteBlock
     @Override
     protected ItemInteractionResult useItemOn(
             ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
-        if(!stack.is(ItemRegistry.BEAT_WAKER.asItem())) return super.useItemOn(stack,state,level,pos,player,hand,hitResult);
-
-        if(!level.isClientSide){
-            int val = state.getValue(SUBTICK);
-            player.displayClientMessage(Component.literal(val/10f + " ticks ("+val * SubTickScheduler.SUBTICK_LENGTH+" ms)").withColor(0xB0B0B0), true);
+        if(!stack.is(BetterTags.Items.BEATWAKERS)) return super.useItemOn(stack,state,level,pos,player,hand,hitResult);
+        InteractionResult result = stack.getItem().useOn(new UseOnContext(level, player, hand, stack, hitResult));
+        switch(result){
+            case SUCCESS -> { return ItemInteractionResult.SUCCESS;}
+            case PASS -> { return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;}
+            default -> { return ItemInteractionResult.FAIL;}
         }
-        return ItemInteractionResult.SUCCESS;
     }
 
     @Override
     protected void attack(BlockState state, Level level, BlockPos pos, Player player) {
         if (!level.isClientSide) {
-            if(!player.getWeaponItem().is(ItemRegistry.BEAT_WAKER))
+            ItemStack item = player.getWeaponItem();
+            if(!item.is(BetterTags.Items.BEATWAKERS))
                 super.attack(state, level, pos, player);
             else{
-                int state_val = state.getValue(SUBTICK);
-                int new_val = (player.isShiftKeyDown() ? state_val + 5 : state_val + 1) % 20;
+                if(item.is(ItemRegistry.BEAT_WAKER)){
+                    int state_val = state.getValue(SUBTICK);
+                    int new_val = (player.isShiftKeyDown() ? state_val + 5 : state_val + 1) % MAX_SUBTICKS;
 
-                level.setBlock(pos, state.setValue(SUBTICK, new_val), Block.UPDATE_ALL);
+                    level.setBlock(pos, state.setValue(SUBTICK, new_val), Block.UPDATE_ALL);
 
-                player.displayClientMessage(Component.literal(new_val/10f + " ticks ("+new_val * SubTickScheduler.SUBTICK_LENGTH+" ms)").withColor(0xB0B0B0), true);
+                    player.displayClientMessage(Component.literal(new_val/10f + " ticks ("+new_val * SubTickScheduler.SUBTICK_LENGTH+" ms)").withColor(0xB0B0B0), true);
+                } else
+                if(item.is(ItemRegistry.NOTE_WAKER)){
+                    player.displayClientMessage(Component.literal(NOTE_STRING[state.getValue(NOTE)]).withColor(0xB030B0), true);
+                    super.attack(state, level, pos, player);
+                }
             }
         }
     }
--- src/main/java/net/nayrus/betterbeats/item/BeatWaker.java
+++ src/main/java/net/nayrus/betterbeats/item/BeatWaker.java
@@ -1,11 +1,19 @@
 package net.nayrus.betterbeats.item;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.nayrus.betterbeats.block.AdvancedNoteblock;
+import net.nayrus.betterbeats.util.SubTickScheduler;
 import org.jetbrains.annotations.NotNull;
 
 public class BeatWaker extends Item {
@@ -26,5 +34,41 @@ public class BeatWaker extends Item {
     @Override
     public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miningEntity) {
         return super.mineBlock(stack, level, state, pos, miningEntity);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        BlockState state = level.getBlockState(pos);
+        ItemStack stack = context.getItemInHand();
+        Player player = context.getPlayer();
+        if(player==null) return InteractionResult.FAIL;
+        if(!(state.getBlock() instanceof AdvancedNoteblock block)) return InteractionResult.PASS;
+        if(!level.isClientSide) {
+
+            if (stack.is(ItemRegistry.BEAT_WAKER)) {
+                int val = state.getValue(AdvancedNoteblock.SUBTICK);
+                player.displayClientMessage(Component.literal(val / 10f + " ticks (" + val * SubTickScheduler.SUBTICK_LENGTH + " ms)").withColor(0xB0B0B0), true);
+                return InteractionResult.SUCCESS;
+            }
+            if (stack.is(ItemRegistry.NOTE_WAKER)) {
+                int old_val = state.getValue(AdvancedNoteblock.NOTE);
+                int new_val = player.isShiftKeyDown() ? (old_val + 5) % 25 : (old_val + 1) % 25;
+                int _new = net.neoforged.neoforge.common.CommonHooks.onNoteChange(level, pos, state, old_val, new_val);
+
+                if (_new == -1) return InteractionResult.FAIL;
+                state = state.setValue(AdvancedNoteblock.NOTE, _new);
+                level.setBlock(pos, state, 3);
+                if (state.getValue(AdvancedNoteblock.INSTRUMENT).worksAboveNoteBlock() || level.getBlockState(pos.above()).isAir()) {
+                    level.blockEvent(pos, block, 0, 0);
+                    level.gameEvent(player, GameEvent.NOTE_BLOCK_PLAY, pos);
+                }
+                player.awardStat(Stats.TUNE_NOTEBLOCK);
+                player.displayClientMessage(Component.literal(AdvancedNoteblock.NOTE_STRING[_new]).withColor(0xB030B0), true);
+                return InteractionResult.SUCCESS;
+            }
+        }
+        return InteractionResult.PASS;
     }
 }
