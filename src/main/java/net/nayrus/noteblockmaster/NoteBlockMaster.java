928f52c9c3cf3eac92827ff7547e113e6ea605a0
LK-HsKo <kami.agofem@yahoo.de>
2025-01-05 19:02:15 +0100

    Add creative mod tab

--- src/main/java/net/nayrus/noteblockmaster/NoteBlockMaster.java
+++ src/main/java/net/nayrus/noteblockmaster/NoteBlockMaster.java
@@ -2,8 +2,6 @@ package net.nayrus.noteblockmaster;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.nayrus.noteblockmaster.block.AdvancedNoteBlock;
 import net.nayrus.noteblockmaster.command.BPMInfoCommand;
@@ -21,7 +19,6 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -35,9 +32,9 @@ public class NoteBlockMaster
 
     public static final String MOD_ID = "noteblockmaster";
 
+    @SuppressWarnings("unused")
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(Registries.RECIPE_SERIALIZER, MOD_ID);
     public static final DeferredHolder<RecipeSerializer<?>, TunerRecipeSerializer> TUNER_RECIPE_SERIALIZER = RECIPE_SERIALIZERS.register("tunerrecipe", () -> TunerRecipeSerializer.INSTANCE);
 
@@ -49,27 +46,14 @@ public class NoteBlockMaster
         NeoForge.EVENT_BUS.register(this);
 
         Registry.register(modEventBus);
-        CREATIVE_MODE_TABS.register(modEventBus);
         RECIPE_SERIALIZERS.register(modEventBus);
 
         modEventBus.addListener(PacketHandler::registerNetwork);
 
         if(FMLEnvironment.dist == Dist.CLIENT){
             NeoForge.EVENT_BUS.register(ClientEvents.class);
-            modEventBus.addListener(this::addCreative);
         }else{
             NeoForge.EVENT_BUS.register(ServerEvents.class);
-        }
-    }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES){
-            event.accept(Registry.TEMPOTUNER);
-            event.accept(Registry.NOTETUNER);
-        }
-        if(event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS){
-            event.accept(Registry.ADVANCED_NOTEBLOCK);
         }
     }
 
--- src/main/java/net/nayrus/noteblockmaster/utils/Registry.java
+++ src/main/java/net/nayrus/noteblockmaster/utils/Registry.java
@@ -1,7 +1,11 @@
 package net.nayrus.noteblockmaster.utils;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -17,6 +21,7 @@ public class Registry
 {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(NoteBlockMaster.MOD_ID);
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(NoteBlockMaster.MOD_ID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, NoteBlockMaster.MOD_ID);
 
     public static final DeferredBlock<Block> ADVANCED_NOTEBLOCK = Registry.BLOCKS.register("advanced_noteblock",
             () -> new AdvancedNoteBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.NOTE_BLOCK).noOcclusion()));
@@ -29,10 +34,17 @@ public class Registry
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
         ITEMS.register(eventBus);
+        CREATIVE_MODE_TABS.register(eventBus);
     }
 
     static{
         Registry.ITEMS.register("advanced_noteblock",
                 ()-> new BlockItem(Registry.ADVANCED_NOTEBLOCK.get(), new Item.Properties()));
+        CREATIVE_MODE_TABS.register("noteblockmaster", ()-> CreativeModeTab.builder()
+                .title(Component.literal("Note Block Master"))
+                .icon(() -> new ItemStack(ADVANCED_NOTEBLOCK.asItem()))
+                .displayItems((pars, output) -> ITEMS.getEntries()
+                        .forEach(item -> output.accept(item.get())))
+                .build());
     }
 }
